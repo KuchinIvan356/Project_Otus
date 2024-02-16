bb12fe34fb782f4e455fd3a713bd87aa4a201c0b
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-02-16 16:57:23 +0300

    Update preinstall_srv_1.sh

--- nginx-SRV1/preinstall_srv_1.sh
+++ nginx-SRV1/preinstall_srv_1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo apt install nginx;
 sudo systemctl start nginx;
-ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa <<< y
-git clone https://github.com/KuchinIvan356/Project_Otus.git
 wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz;
 tar xzvf node_exporter-*.t*gz;
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin;

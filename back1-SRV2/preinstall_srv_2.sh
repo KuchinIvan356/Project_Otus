66ef4b7d653182556a7f93f89cf0f3e5bb0a59e4
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-02-26 10:23:46 +0300

    Update preinstall_srv_2.sh

--- back1-SRV2/preinstall_srv_2.sh
+++ back1-SRV2/preinstall_srv_2.sh
@@ -4,7 +4,7 @@ sudo apt install apache2;
 sudo systemctl start apache2;
 sudo apt install mysql-server-8.0;
 sudo systemctl start mysql;
-wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz;
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
 tar xzvf node_exporter-*.t*gz;
 useradd --no-create-home --shell /bin/false node_exporter;
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin;

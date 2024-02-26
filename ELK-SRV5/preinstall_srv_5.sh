2eda0e481e6f21fa0e4cddf458aa1338d781ebfa
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-02-26 11:38:10 +0300

    Update preinstall_srv_5.sh

--- ELK-SRV5/preinstall_srv_5.sh
+++ ELK-SRV5/preinstall_srv_5.sh
@@ -9,6 +9,10 @@ wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/e/elasticsearch/ela
 wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/k/kibana/kibana-8.9.1-amd64.deb;
 wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/l/logstash/logstash-8.9.1-amd64.deb;
 sudo dpkg -i elasticsearch*.deb;
+sudo systemctl enable --now elasticsearch.service
+/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
+/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana
+/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node
 sudo dpkg -i kibana*.deb;
 sudo dpkg -i logstash*.deb;
 systemctl daemon-reload;
@@ -16,8 +20,5 @@ systemctl enable --now elasticsearch;
 systemctl enable --now kibana;
 systemctl enable --now logstash;
 systemctl enable --now prometheus-node-exporter;
-/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic;
-/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana;
-/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node
 cp /root/Project_Otus/ELK-SRV5/10-eth.network /etc/systemd/network/10-eth.network
 systemctl restart systemd-networkd

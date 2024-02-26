e7f88f0180e16c9a4bf1c163de65f4eced03a7d7
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-02-26 10:14:07 +0300

    Update preinstall_srv_1.sh

--- nginx-SRV1/preinstall_srv_1.sh
+++ nginx-SRV1/preinstall_srv_1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt install nginx;
 sudo systemctl start nginx;
-wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz;
-tar xzvf node_exporter-*.t*gz;
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar xzvf node_exporter-*.t*gz
+useradd --no-create-home --shell /bin/false node_exporter
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin;
 chown node_exporter: /usr/local/bin/node_exporter;
 wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/f/filebeat/filebeat-8.9.1-amd64.deb;

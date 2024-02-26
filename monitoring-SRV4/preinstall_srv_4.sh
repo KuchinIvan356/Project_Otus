4a1b346a625811afe0e81b00d3883a1283aae1d5
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-02-26 10:29:48 +0300

    Update preinstall_srv_4.sh

--- monitoring-SRV4/preinstall_srv_4.sh
+++ monitoring-SRV4/preinstall_srv_4.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 wget https://github.com/prometheus/prometheus/releases/download/v2.46.0/prometheus-2.46.0.linux-amd64.tar.gz;
 tar xzvf prometheus-*.t*gz;
+useradd --no-create-home --shell /usr/sbin/nologin prometheus
 mkdir {/etc/,/var/lib/}prometheus;
 cp -vi prometheus-*.linux-amd64/prom{etheus,tool} /usr/local/bin;
 cp -rvi prometheus-*.linux-amd64/{console{_libraries,s},prometheus.yml} /etc/prometheus/;
 chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib/prometheus/;
 wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz;
 tar xzvf node_exporter-*.t*gz;
+useradd --no-create-home --shell /bin/false node_exporter
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin;
 chown node_exporter: /usr/local/bin/node_exporter;
 apt --fix-broken -y install

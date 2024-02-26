12af16d0990082dafd246bd27eeda323018b90bf
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-02-26 11:00:14 +0300

    Update preinstall_srv_5.sh

--- ELK-SRV5/preinstall_srv_5.sh
+++ ELK-SRV5/preinstall_srv_5.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt install bash-completion git prometheus-node-exporter -y;
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar xzvf node_exporter-*.t*gz
 useradd --no-create-home --shell /bin/false node_exporter;
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin;
 chown node_exporter: /usr/local/bin/node_exporter;

c79569426b855b9b75b8f88022860dd6a9ef9daa
Ivan Kuchin <149145333+KuchinIvan356@users.noreply.github.com>
2024-04-03 22:29:37 +0300

    Update preinstall_srv_5.sh

--- ELK-SRV5/preinstall_srv_5.sh
+++ ELK-SRV5/preinstall_srv_5.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt-get update -y;
-wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
-tar xzvf node_exporter-*.t*gz
+apt install bash-completion git prometheus-node-exporter -y;
 useradd --no-create-home --shell /bin/false node_exporter;
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin;
 chown node_exporter: /usr/local/bin/node_exporter;
@@ -10,20 +9,18 @@ wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/e/elasticsearch/ela
 wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/k/kibana/kibana-8.9.1-amd64.deb;
 wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/l/logstash/logstash-8.9.1-amd64.deb;
 sudo dpkg -i elasticsearch*.deb;
-sudo systemctl enable --now elasticsearch.service
-/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
-/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana
-/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node
 sudo dpkg -i kibana*.deb;
 sudo dpkg -i logstash*.deb;
-rm -r elasticsearch-8.9.1-amd64.deb;
-rm -r kibana-8.9.1-amd64.deb;
-rm -r logstash-8.9.1-amd64.deb;
 systemctl daemon-reload;
 systemctl enable --now elasticsearch;
 systemctl enable --now kibana;
 systemctl enable --now logstash;
 systemctl enable --now prometheus-node-exporter;
-systemctl restart elasticsearch.service;
+/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic;
+/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana;
+/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node
+rm -r elasticsearch-8.9.1-amd64.deb;
+rm -r kibana-8.9.1-amd64.deb;
+rm -r logstash-8.9.1-amd64.deb;
 cp /root/Project_Otus/ELK-SRV5/10-eth.network /etc/systemd/network/10-eth.network
 systemctl restart systemd-networkd
